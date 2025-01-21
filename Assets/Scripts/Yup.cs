0aa62c36a79289c6202ffc9b63aaf5fe9b1ec2bd
Ioni <sami.repo@live.com>
2025-01-21 20:25:21 +0200

    Updated settings and added packages

---
+++ Assets/Scripts/Yup.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace GGJ
+{
+    public class Yup : MonoBehaviour
+    {
+        public void OnFight(InputValue value)
+        {
+            Debug.Log("yup");
+        }
+    }
+}
